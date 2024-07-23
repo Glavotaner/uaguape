c4d7e17b3dc35afb37c2eab1268f5a88c9a85352
marin <marin.glavas@outlook.com>
2024-07-23 12:26:41 +0200

    setup placeholder names

--- apps/backend/src/configuration/configuration.ts
+++ apps/backend/src/configuration/configuration.ts
@@ -1,3 +1,5 @@
 export default () => ({
   JWT_SECRET: process.env.JWT_SECRET,
+  PERSON_1: process.env.PERSON_1,
+  PERSON_2: process.env.PERSON_2,
 });
--- apps/backend/src/user/user.service.ts
+++ apps/backend/src/user/user.service.ts
@@ -2,21 +2,29 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDto, UpdateUserDto } from 'uaguape-common';
 import { Prisma } from '@prisma/client';
 import { PrismaService } from 'uaguape-db';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class UserService {
   private readonly _user: Prisma.UserDelegate;
 
-  constructor(prismaService: PrismaService) {
+  constructor(
+    prismaService: PrismaService,
+    private readonly configService: ConfigService,
+  ) {
     this._user = prismaService.user;
   }
 
-  create(createUserDto: CreateUserDto) {
+  create(dto: CreateUserDto) {
+    // TODO fix oauth scopes
+    const name = dto.email.includes('marin')
+      ? this.configService.get('PERSON_2')
+      : this.configService.get('PERSON_1');
     // TODO make only create
     return this._user.upsert({
-      create: { name: createUserDto.name, email: createUserDto.email },
-      update: { name: createUserDto.name, email: createUserDto.email },
-      where: { email: createUserDto.email },
+      create: { ...dto, name },
+      update: { ...dto, name },
+      where: { email: dto.email },
     });
   }
 
